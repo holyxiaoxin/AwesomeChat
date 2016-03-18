af59ace21658d7e41bd5ea1a4d7b49d3b53cc280
Jia Rong Lim <jr.loves.maths@gmail.com>
2016-03-18 13:36:35 +0800

    Fix failing test

--- src/__specs__/Chat.spec.js
+++ src/__specs__/Chat.spec.js
@@ -10,14 +10,14 @@ describe("<Chat/>", () => {
   context('when state is loading', () => {
     it('should render loading text', () => {
       const Text = wrapper.find('Text');
-      
+
       expect(Text).to.have.length(1);
     });
   });
 
   context('when state has finish loading', () => {
     it('should render view with scrollview', () => {
-      // wrapper.setState({loaded: true});
+      wrapper.setState({loaded: true});
       const View = wrapper.find('View');
 
       expect(View).to.have.length(2);
